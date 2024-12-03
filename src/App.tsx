64339a9de0cf4c9d8b521aa04909da893737a696
Oguzhan Ozgul <oguzhanozgul@gmail.com>
2024-12-03 14:32:38 +0100

    Add cleanup before app unload.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router";
 
+import audioPlayerInstance from "./components/AudioPlayer/AudioPlayer";
 import StickyPlayerContainer from "./components/Player/StickyPlayerContainer";
 import PageIndex from "./pages/PageIndex";
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<PageIndex />} />));
+
+  window.addEventListener("beforeunload", () => {
+    audioPlayerInstance.cleanup();
+  });
 
   return (
     <>
